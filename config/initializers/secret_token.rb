850fd85399393c1ef7fc78cffb5cfaf556e4675a
Davis Allen <davis.b.allen@gmail.com>
2011-06-11 00:23:50 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adslapme::Application.config.secret_token = 'd715148d3f8de416dbde5e70937c6ec155e31594f0d5c04b2a6fbd820fcbdd9a7da9d8515c911f499a17a7bd615c4da5e1b84df0b72a1b293e47f37c48a6dff0'
