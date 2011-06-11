c85733f99fe9ad6963086cce8666cac4dc828f4a
Davis Allen <davis.b.allen@gmail.com>
2011-06-11 11:28:34 -0400

    Added Settings config

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
   def new
-    @oauth = Koala::Facebook::OAuth.new(103530109716629, "8e9f233a2b27f508ae24ca0b58c2db2a", "http://3pcq.localtunnel.com/sessions/facebook_session_create")
+    @oauth = Koala::Facebook::OAuth.new(Settings.facebook.app_id, Settings.facebook.app_secret, "#{Settings.app_url}/sessions/facebook_session_create")
     facebook_login_url = @oauth.url_for_oauth_code
     redirect_to facebook_login_url
   end
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
 
   def facebook_session_create
     code = params[:code]
-    @oauth = Koala::Facebook::OAuth.new(103530109716629, "8e9f233a2b27f508ae24ca0b58c2db2a", "http://3pcq.localtunnel.com/sessions/facebook_session_create")
+    @oauth = Koala::Facebook::OAuth.new(Settings.facebook.app_id, Settings.facebook.app_secret, "#{Settings.app_url}/sessions/facebook_session_create")
     facebook_token = @oauth.get_access_token(code)
     graph = Koala::Facebook::GraphAPI.new(facebook_token)
     puts "#{facebook_token}"
